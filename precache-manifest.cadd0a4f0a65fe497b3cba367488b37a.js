173efbf7b63707788dfebbe783e9eff95e1bf334
Devansh <91583975+Devansh-bharadwaj@users.noreply.github.com>
2022-03-09 23:22:08 +0530

    Updates

---
+++ precache-manifest.cadd0a4f0a65fe497b3cba367488b37a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7764a4ccc469632e460e973207f8100d",
+    "url": "/BookMyRoom/index.html"
+  },
+  {
+    "revision": "46be0199bf282c7df152",
+    "url": "/BookMyRoom/static/css/main.f2f5a8fe.chunk.css"
+  },
+  {
+    "revision": "ff1d8bfe30d4c5b0a3d7",
+    "url": "/BookMyRoom/static/js/2.aaaab038.chunk.js"
+  },
+  {
+    "revision": "4aa839f25784e6ec59906b932c7e8c9f",
+    "url": "/BookMyRoom/static/js/2.aaaab038.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "79787f980c1912209aaa",
+    "url": "/BookMyRoom/static/js/3.2ce24e48.chunk.js"
+  },
+  {
+    "revision": "46be0199bf282c7df152",
+    "url": "/BookMyRoom/static/js/main.a35d738d.chunk.js"
+  },
+  {
+    "revision": "98e4d9897409e9c05713",
+    "url": "/BookMyRoom/static/js/runtime-main.63d8d56e.js"
+  }
+]);
